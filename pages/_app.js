54e82156bf8ed66fb946975f753b059259d49e43
suryawiguna <suryawigunaa@gmail.com>
2022-07-18 12:58:42 +0800

    add GA

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,10 @@ import Head from "next/head";
 import Layout from "../components/layout";
 import dynamic from "next/dynamic";
 
+import * as ga from "../lib/ga";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 const TopProgressBar = dynamic(
   () => {
     return import("../components/topProgressBar");
@@ -12,6 +16,22 @@ const TopProgressBar = dynamic(
 );
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
