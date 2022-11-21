6db6e4548df840acfd3ded5e5b10c0410160766a
suryawiguna <suryawigunaa@gmail.com>
2022-11-21 18:34:41 +0800

    set experience page on storyblok

--- pages/experience.jsx
+++ pages/experience.jsx
@@ -1,32 +1,29 @@
 import Head from "next/head";
 import Layout from "../components/layout";
-import { getNavigation } from "../lib/api";
+import { getNavigation, getPage } from "../lib/api";
 
 export default function Experience({ data, navigation }) {
-  const { experience } = data;
   return (
     <Layout navigation={navigation}>
       <Head>
-        <title>{`${experience.title} - Surya Wiguna`}</title>
+        <title>{`${data.title} - Surya Wiguna`}</title>
       </Head>
       <div className="flex flex-col gap-8">
-        <h1 className="font-bold text-3xl">{experience.title}</h1>
+        <h1 className="font-bold text-3xl">{data.title}</h1>
         <ol className="relative border-l border-gray-300">
-          {experience.histories.map((item, key) => {
+          {data.body.map((item, key) => {
             return (
               <li
                 key={key}
-                className={`${
-                  key < experience.histories.length - 1 && "mb-12"
-                } ml-4`}
+                className={`${key < data.body.length - 1 && "mb-12"} ml-4`}
               >
                 <div
                   className={`absolute w-5 h-5 ${
-                    item.year.includes("now") ? "bg-gray-900" : "bg-gray-300"
+                    item.periode.includes("now") ? "bg-gray-900" : "bg-gray-300"
                   } rounded-full mt-1 -left-2.5 border-4 border-white`}
                 />
                 <time className="mb-1 text-xs font-normal leading-none text-gray-400 dark:text-gray-500">
-                  {item.year}
+                  {item.periode}
                 </time>
                 <h3 className="text-lg font-semibold">{item.title}</h3>
                 <p className="mb-4 text-sm font-normal text-gray-600">
@@ -42,7 +39,7 @@ export default function Experience({ data, navigation }) {
 }
 
 export async function getStaticProps() {
-  const data = require("/data/data.json");
+  const data = (await getPage("experience")) || [];
   const navigation = (await getNavigation()) || [];
 
   return {
