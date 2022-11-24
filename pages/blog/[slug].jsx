5f84607f35761c1ea71c14e8687155c6726a8da3
suryawiguna <suryawigunaa@gmail.com>
2022-11-24 14:33:44 +0800

    fix share wrong link

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -12,8 +12,20 @@ import Link from "next/link";
 // import { GoogleAdsenseContainer } from "../../components/ads";
 import { InlineShareButtons, InlineReactionButtons } from "sharethis-reactjs";
 import DisqusComments from "../../components/disqusComment";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const Post = ({ data, post, featuredPosts, navigation }) => {
+  const router = useRouter();
+  const [showShare, setShowShare] = useState(false);
+
+  useEffect(() => {
+    setShowShare(false);
+    setTimeout(() => {
+      setShowShare(true);
+    }, 500);
+  }, [router.asPath]);
+
   return (
     <Layout data={data} navigation={navigation}>
       <div className="flex flex-col lg:flex-row gap-10">
@@ -72,27 +84,53 @@ const Post = ({ data, post, featuredPosts, navigation }) => {
           <div className="flex flex-col gap-2">
             <h3 className="text-xl font-bold px-2">🙌🏼 Share This</h3>
             <div className="ml-2">
-              <InlineShareButtons
-                config={{
-                  alignment: "left", // alignment of buttons (left, center, right)
-                  color: "social", // set the color of buttons (social, white)
-                  enabled: true, // show/hide buttons (true, false)
-                  font_size: 18, // font size for the buttons
-                  labels: "null", // button labels (cta, counts, null)
-                  language: "en", // which language to use (see LANGUAGES)
-                  networks: [
-                    // which networks to include (see SHARING NETWORKS)
-                    "facebook",
-                    "linkedin",
-                    "twitter",
-                    "whatsapp",
-                  ],
-                  padding: 10, // padding within buttons (INTEGER)
-                  radius: 10, // the corner radius on each button (INTEGER)
-                  show_total: false,
-                  size: 40, // the size of each button (INTEGER)
-                }}
-              />
+              {showShare ? (
+                <InlineShareButtons
+                  config={{
+                    alignment: "left", // alignment of buttons (left, center, right)
+                    color: "social", // set the color of buttons (social, white)
+                    enabled: true, // show/hide buttons (true, false)
+                    font_size: 18, // font size for the buttons
+                    labels: "null", // button labels (cta, counts, null)
+                    language: "en", // which language to use (see LANGUAGES)
+                    networks: [
+                      // which networks to include (see SHARING NETWORKS)
+                      "facebook",
+                      "linkedin",
+                      "twitter",
+                      "whatsapp",
+                    ],
+                    padding: 10, // padding within buttons (INTEGER)
+                    radius: 100, // the corner radius on each button (INTEGER)
+                    show_total: false,
+                    size: 40, // the size of each button (INTEGER)
+                    url: `https://suryawiguna.com${router.asPath}`,
+                  }}
+                />
+              ) : (
+                <span>
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-10 w-10 text-gray-700"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                </span>
+              )}
             </div>
           </div>
           <div className="flex flex-col gap-2">
