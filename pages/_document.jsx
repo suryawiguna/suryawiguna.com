c821dfddb6189f9d8c5fc0ce6948fbf4c587093f
suryawiguna <suryawigunaa@gmail.com>
2022-11-24 10:14:37 +0800

    change to crossOrigin

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -26,7 +26,7 @@ export default class MyDocument extends Document {
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8562944824984533"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
           ></script>
         </Head>
         <body>
