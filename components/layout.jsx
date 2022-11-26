7e00d84fa80ea328c2a5d431cbf8085e7230099d
suryawiguna <suryawigunaa@gmail.com>
2022-11-26 21:47:15 +0800

    add layout on 404 page

--- components/layout.jsx
+++ components/layout.jsx
@@ -4,21 +4,26 @@ import Navigation from "./navigation";
 export default function Layout({ data, navigation, children }) {
   return (
     <>
-      <Head>
-        <title>{`${data.title} - Surya Wiguna`}</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          property="og:title"
-          content={`${data.seo.title} - Surya Wiguna`}
-          key="title"
-        />
-        <meta
-          property="og:description"
-          content={data.seo.description}
-          key="description"
-        />
-        <meta property="og:image" content={data.seo.og_image} />
-      </Head>
+      {data && (
+        <Head>
+          <title>{`${data.title} - Surya Wiguna`}</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta
+            property="og:title"
+            content={`${data.seo.title} - Surya Wiguna`}
+            key="title"
+          />
+          <meta
+            property="og:description"
+            content={data.seo.description}
+            key="description"
+          />
+          <meta property="og:image" content={data.seo.og_image} />
+        </Head>
+      )}
       <div className="lg:container h-full mx-auto lg:px-10 2xl:px-52">
         <div className="flex flex-col md:flex-row gap-4 justify-center">
           <Navigation navigation={navigation} />
