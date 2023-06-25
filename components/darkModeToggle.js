ac7b1d8d8ed792fb6055c45fe8177fab9932b25d
suryawiguna <suryawigunaa@gmail.com>
2023-06-25 18:12:10 +0800

    fix dark mode toggle hydration

---
+++ components/darkModeToggle.js
@@ -0,0 +1,43 @@
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function DarkModeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <label htmlFor="toggleThree" className="cursor-pointer">
+      <div
+        className="relative h-8 w-14 mx-3 md:m-5"
+        onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
+      >
+        <div className="block h-8 w-14 rounded-full bg-zinc-200 dark:bg-zinc-700"></div>
+        <div
+          className={`dot absolute transition-all translate-x mx-1 ${
+            theme != "light" ? "translate-x-6" : ""
+          } top-1 flex h-6 w-6 items-center justify-center rounded-full ${
+            theme != "light" ? "bg-slate-800" : "bg-amber-500"
+          }`}
+        >
+          {/* ICON */}
+          <span>
+            {theme != "light" ? (
+              <i className="bx bxs-moon text-zinc-100 text-sm"></i>
+            ) : (
+              <i className="bx bxs-sun text-zinc-100 text-sm"></i>
+            )}
+          </span>
+        </div>
+      </div>
+    </label>
+  );
+}
--- components/navigation.jsx
+++ components/navigation.jsx
@@ -1,12 +1,11 @@
-import { useTheme } from "next-themes";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import DarkModeToggle from "./darkModeToggle";
 
 export default function Navigation({ navigation }) {
   const router = useRouter();
   const [menuOpen, setMenuOpen] = useState(false);
-  const { theme, setTheme } = useTheme();
 
   return (
     <nav className="w-full z-10 md:w-4/12 lg:w-3/12 xl:w-2/12 p-0 lg:p-4 md:h-screen fixed top-0 md:sticky bg-white drop-shadow-sm md:drop-shadow-none dark:bg-zinc-900 dark:text-gray-300">
@@ -16,7 +15,7 @@ export default function Navigation({ navigation }) {
             <span className="font-bold mr-1">Surya</span>
             <span className="font-light">Wiguna</span>
           </p>
-          {DarkModeToggle(theme, setTheme)}
+          {DarkModeToggle()}
           <button
             className={`flex items-center text-md${
               menuOpen ? " bg-amber-100 dark:bg-amber-900" : " bg-transparent"
@@ -51,40 +50,9 @@ export default function Navigation({ navigation }) {
               </Link>
             );
           })}
-          <span className="hidden md:block">
-            {DarkModeToggle(theme, setTheme)}
-          </span>
+          <span className="hidden md:block">{DarkModeToggle()}</span>
         </div>
       </div>
     </nav>
-  );
-}
-
-function DarkModeToggle(theme, setTheme) {
-  return (
-    <label htmlFor="toggleThree" className="cursor-pointer">
-      <div
-        className="relative h-8 w-14 mx-3 md:m-5"
-        onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
-      >
-        <div className="block h-8 w-14 rounded-full bg-zinc-200 dark:bg-zinc-700"></div>
-        <div
-          className={`dot absolute transition-all translate-x mx-1 ${
-            theme == "dark" ? "translate-x-6" : ""
-          } top-1 flex h-6 w-6 items-center justify-center rounded-full ${
-            theme == "dark" ? "bg-slate-800" : "bg-amber-500"
-          }`}
-        >
-          {/* ICON */}
-          <span>
-            {theme && theme == "light" ? (
-              <i className="bx bxs-sun text-zinc-100 text-sm"></i>
-            ) : (
-              <i className="bx bxs-moon text-zinc-100 text-sm"></i>
-            )}
-          </span>
-        </div>
-      </div>
-    </label>
   );
 }
