0c7b36ff63d3c712e60a98a3ed5eebc3141dacde
suryawiguna <suryawigunaa@gmail.com>
2023-07-01 15:41:02 +0800

    edit link page

--- components/link/introduction.jsx
+++ components/link/introduction.jsx
@@ -3,17 +3,20 @@ import { RichText } from "../global";
 
 export default function Introduction({ blok }) {
   return (
-    <section className="flex items-center">
+    <section className="flex flex-col md:flex-row items-center">
       <Image
         src={blok.image.filename}
         alt=""
-        width={60}
-        height={60}
+        width={84}
+        height={84}
         placeholder="blur"
         blurDataURL={`${blok.image.filename}/m/40x40`}
         className="rounded-full"
       />
-      <RichText data={blok.description} className="text-lg ml-3" />
+      <RichText
+        data={blok.description}
+        className="text-lg text-center md:text-left mt-2 ml-3"
+      />
     </section>
   );
 }
--- components/link/links.jsx
+++ components/link/links.jsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Links({ blok }) {
   return (
     <section className="flex flex-col gap-5">
@@ -10,20 +8,9 @@ export default function Links({ blok }) {
             href={link.link.url}
             target="_blank"
             rel="noreferrer"
-            className="bg-zinc-100 hover:bg-zinc-200 dark:bg-zinc-700 hover:scale-[0.98] transition-transform  py-3 px-5 flex items-center gap-2 rounded-full max-w-md"
+            className="bg-zinc-100 hover:bg-zinc-200 dark:bg-zinc-700 dark:hover:bg-zinc-600 hover:scale-[0.98] transition-transform py-4 px-5 text-center gap-2 rounded-full max-w-md"
           >
-            <span className="flex justify-center items-center gap-2">
-              <Image
-                src={link.icon.filename}
-                alt=""
-                width={20}
-                height={20}
-                placeholder="blur"
-                blurDataURL={`${link.icon.filename}/m/40x40`}
-                className="w-[24px] h-[24px]"
-              />
-              <span className="flex-1">{link.name}</span>
-            </span>
+            {link.name}
           </a>
         );
       })}
