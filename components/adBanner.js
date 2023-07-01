55695f9c4416a54ccbf44aaa9a43ce67c24cf90c
suryawiguna <suryawigunaa@gmail.com>
2023-07-01 14:48:52 +0800

    add script

--- components/adBanner.js
+++ components/adBanner.js
@@ -10,14 +10,22 @@ const AdBanner = () => {
   }, []);
 
   return (
-    <ins
-      class="adsbygoogle"
-      style={{ display: "block" }}
-      data-ad-client="ca-pub-8562944824984533"
-      data-ad-slot="5983640257"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    ></ins>
+    <>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8562944824984533"
+        crossorigin="anonymous"
+      ></script>
+      <ins
+        class="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-8562944824984533"
+        data-ad-slot="5983640257"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+    </>
   );
 };
 
