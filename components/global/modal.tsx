ffba29ebda308216d6f61be797404e851a5c7593
suryawiguna <suryawigunaa@gmail.com>
2024-04-26 09:00:15 +0800

    fix portfolio modal

--- components/global/modal.tsx
+++ components/global/modal.tsx
@@ -1,17 +1,9 @@
 "use client";
 
-import styled from "styled-components";
-
-const ModalDiv = styled.div`
-  &::-webkit-scrollbar {
-    display: none !important;
-  }
-`;
-
 export default function Modal({ children }) {
   return (
-    <dialog id="portfolio-modal" className="modal">
-      <ModalDiv className="modal-box p-8 flex flex-col justify-between">
+    <dialog id="portfolio-modal" className="rounded-xl backdrop:bg-black/50">
+      <div className="modal-box max-w-xl p-8 flex flex-col justify-between">
         <form method="dialog">
           <button
             className="btn btn-sm btn-circle btn-ghost absolute right-3 top-3"
@@ -21,10 +13,7 @@ export default function Modal({ children }) {
           </button>
         </form>
         <div className="flex flex-col gap-4">{children}</div>
-      </ModalDiv>
-      <form method="dialog" className="modal-backdrop">
-        <button>close</button>
-      </form>
+      </div>
     </dialog>
   );
 }
