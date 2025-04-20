ebb5a43bc95f39533bdf2534cca074bda3b6b6a5
suryawiguna <suryawigunaa@gmail.com>
2025-04-20 17:15:10 +0800

    feat(blog): show tags

--- components/blog/fullPost.tsx
+++ components/blog/fullPost.tsx
@@ -11,9 +11,24 @@ export default function FullPost({ post }) {
     <div className="flex flex-col gap-6">
       <div>
         <h1 className="text-4xl">{post.name}</h1>
-        <p className="text-gray-400 text-sm mt-2">
+        <p className="text-gray-500 text-sm mt-2">
           Published on {moment(post.first_published_at).format("MMMM DD, YYYY")}
         </p>
+        {post.tag_list.length > 0 && (
+          <div className="mt-2 max-w-screen">
+            {post.tag_list.map((tag: string, index: number) => {
+              return (
+                <div
+                  key={index}
+                  className="inline-block text-xs bg-gray-200 text-gray-500 px-2 py-1 rounded-md"
+                  style={{ margin: "2px" }}
+                >
+                  #{tag}
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
       <div className="w-full">
         <Image
--- components/blog/postCard.tsx
+++ components/blog/postCard.tsx
@@ -12,7 +12,7 @@ export default function PostCard({ post, show }) {
       className={`${show ? "" : " hidden"}`}
     >
       <div className="flex flex-col sm:flex-row items-stretch gap-4">
-        <div className="relative min-h-[180px] min-w-[140px] sm:min-h-[140px] sm:min-w-[180px]">
+        <div className="relative min-h-[180px] min-w-[140px] sm:min-h-[140px] sm:min-w-[240px]">
           <Image
             src={post.content.featured_image.filename}
             alt={post.content.featured_image.alt}
@@ -30,10 +30,25 @@ export default function PostCard({ post, show }) {
               data={post.content.excerpt}
             />
           </div>
-          <p className="published_date text-xs self-end font-light text-zinc-500 mt-4 tracking-wide">
+          <p className="published_date text-xs self-end font-light text-zinc-500 mt-2 tracking-wide">
             Published on{" "}
             {moment(post.first_published_at).format("MMMM DD, YYYY")}
           </p>
+          {post.tag_list.length > 0 && (
+            <div className="mt-2 max-w-screen">
+              {post.tag_list.map((tag: string, index: number) => {
+                return (
+                  <div
+                    key={index}
+                    className="inline-block text-xs bg-gray-200 text-gray-500 px-2 py-1 rounded-md"
+                    style={{ margin: "2px" }}
+                  >
+                    #{tag}
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
       </div>
     </Link>
